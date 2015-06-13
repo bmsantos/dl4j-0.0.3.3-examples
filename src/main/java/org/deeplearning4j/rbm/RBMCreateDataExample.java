38b1c388114273db35a1278994f4a0694e024448
nyghtowl <warrick.melanie@gmail.com>
2015-06-12 17:42:17 -0700

    Setup to run NeuralNetPlotter directly for graphs. For more indepth view, Listener should be used and needs validation.

--- src/main/java/org/deeplearning4j/rbm/RBMCreateDataExample.java
+++ src/main/java/org/deeplearning4j/rbm/RBMCreateDataExample.java
@@ -1,11 +1,13 @@
 package org.deeplearning4j.rbm;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.deeplearning4j.nn.api.Layer;
 import org.deeplearning4j.nn.conf.layers.RBM;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.rng.DefaultRandom;
 import org.deeplearning4j.nn.gradient.Gradient;
 import org.deeplearning4j.nn.layers.factory.LayerFactories;
 import org.deeplearning4j.nn.params.DefaultParamInitializer;
@@ -13,6 +15,8 @@ import org.deeplearning4j.nn.params.PretrainParamInitializer;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.api.IterationListener;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.plot.FilterRenderer;
+import org.deeplearning4j.plot.NeuralNetPlotter;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.factory.Nd4j;
@@ -59,7 +63,7 @@ public class RBMCreateDataExample {
     public static void main(String... args) throws Exception {
         int numFeatures = 40;
 
-        log.info("Load data....");
+        log.info("Load dat....");
         //        MersenneTwister gen = new MersenneTwister(123); // other data to test?
 
         INDArray input = Nd4j.create(2, numFeatures); // have to be at least two or else output layer gradient is a scalar and cause exception
@@ -78,11 +82,14 @@ public class RBMCreateDataExample {
 
         DataSet trainingSet = new DataSet(input, labels);
 
+        Nd4j.getRandom().setSeed(123);
+
         log.info("Build model....");
         NeuralNetConfiguration conf = new NeuralNetConfiguration.Builder()
                 .layer(new RBM())
                 .nIn(trainingSet.numInputs())
                 .nOut(trainingSet.numOutcomes())
+                .rng(new DefaultRandom(123))
                 .weightInit(WeightInit.SIZE)
                 .constrainGradientToUnitNorm(true)
                 .iterations(3)
@@ -98,48 +105,9 @@ public class RBMCreateDataExample {
 
         log.info("Train model....");
         model.fit(trainingSet.getFeatureMatrix());
-
-        ///////////////////////////
-        Gradient gradient = model.gradient();
-        log.info("PARAMS" + model.getParam("W"));
-        log.info("GRADIENTS" + gradient);
-
-        // Gradient titles - W, W-gradient, b, b-gradient, vb, vb-gradient
-        Set<String> vars = new TreeSet<>(gradient.gradientForVariable().keySet()); // W, b, vb
-        List<String> titles = new ArrayList<>(vars);
-        for(String s : vars) {
-            titles.add(s + "-gradient");
-        }
-
-        // Gradient matrices
-        INDArray[] matrices = new INDArray[]{
-                model.getParam(DefaultParamInitializer.WEIGHT_KEY),
-                model.getParam(PretrainParamInitializer.BIAS_KEY),
-                model.getParam(PretrainParamInitializer.VISIBLE_BIAS_KEY),
-                gradient.gradientForVariable().get(DefaultParamInitializer.WEIGHT_KEY),
-                gradient.gradientForVariable().get(DefaultParamInitializer.BIAS_KEY),
-                gradient.gradientForVariable().get(PretrainParamInitializer.VISIBLE_BIAS_KEY)
-        };
-
-        String[] path = new String[titles.size() + matrices.length];
-        for(int i = 0; i < path.length - 1; i+=2) {
-            path[i] = writeMatrix(matrices[i/2].ravel());
-            path[i+1] = titles.get(i/2);
-        }
-        String paths = StringUtils.join(path, ",");
-//        String plotFile = "~/Documents/skymind/examples/dl4j-0.0.3.3-examples/src/main/resources/python/plot.py";
-        String plotFile = new ClassPathResource("python/plot.py").getFile().getAbsolutePath();
-        String cmd = "python " + plotFile + " multi "+ paths;
-
-        log.info("Rendering Matrix histograms. Close window when ready ton continue or end program... ");
-        Process is = Runtime.getRuntime().exec(cmd);
-
-        log.info("Std out " + IOUtils.readLines(is.getInputStream()).toString());
-        log.error("Std error " + IOUtils.readLines(is.getErrorStream()).toString());
-
-
-        // Single layer just learns features and can't be supervised. Thus cannot be evaluated.
-
-
+               // Visualize End Results
+        NeuralNetPlotter plotter = new NeuralNetPlotter();
+        plotter.plotNetworkGradient(model, model.gradient(), 10);
     }
+
 }
